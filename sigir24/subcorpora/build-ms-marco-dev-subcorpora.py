b29f5aa490de9fe2fcaa2f83d9611b112158384b
Maik Fröbe <maik.froebe@uni-jena.de>
2024-06-25 06:03:19 +0200

    code for subcorpora sampling

---
+++ sigir24/subcorpora/build-ms-marco-dev-subcorpora.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#export IR_DATASETS_HOME=/mnt/ceph/tira/state/ir_datasets/
+import ir_datasets
+import pandas as pd
+from trectools import TrecRun, TrecPoolMaker
+from glob import glob
+from tqdm import tqdm
+import random
+
+queries = {
+    'trec-dl-2019': {i.query_id: i.default_text() for i in ir_datasets.load("msmarco-passage/trec-dl-2019/judged").queries_iter()},
+    'trec-dl-2020': {i.query_id: i.default_text() for i in ir_datasets.load("msmarco-passage/trec-dl-2020/judged").queries_iter()}
+}
+
+docsstore = ir_datasets.load("msmarco-passage").docs_store()
+
+def all_runs(track):
+    runs = []
+    for r in tqdm(glob(f'runs/{track}/*'), f'Parse runs from {track}'):
+        runs += [TrecRun(r)]
+    return runs
+
+ret = {}
+for track in queries:
+    runs = all_runs(track)
+    ret[track] = {}
+    for pool_size in tqdm([5, 10, 20, 50, 100, 1000], 'Pool sizes'):
+        queries_pool = list(set([i for i in pool.keys() if i in queries[track]]))
+        pool = TrecPoolMaker().make_pool(runs, strategy="topX", topX=pool_size).pool
+        ret[track][pool_size] = {i: pool[i] for i in pool.keys() if i in queries_pool}
+
+json.dumps(ret, open('document-dimensions-pool.json', 'w'))
+

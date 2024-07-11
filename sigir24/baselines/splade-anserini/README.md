a64e5d6e16a119b3dc8898022dff16803d57c58f
Maik Fröbe <maik.froebe@uni-jena.de>
2024-07-11 16:39:27 +0200

    merge

--- sigir24/baselines/splade-anserini/README.md
+++ sigir24/baselines/splade-anserini/README.md
@@ -20,13 +20,13 @@ tira-run \
 ```
 
 ## Encoding of the Documents and Queries
-docker build -t anserini-splade .
+docker build -t mam10eks/anserini-splade .
 
 docker run --rm -ti -v ${PWD}:/app -w /app --entrypoint bash anserini-splade
 
 ```
 tira-run \
-	--image anserini-splade \
+	--image mam10eks/anserini-splade \
 	--input-dataset reneuir-2024/tiny-sample-20231030-training
 ```
 
--- sigir24/zenodo/foo.py
+++ sigir24/zenodo/foo.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 from tira.profiling_integration import ProfilingIntegration
 from pathlib import Path
+from tqdm import tqdm
 import pandas as pd
 import json
 
+def _run_dir(user_id, dataset_id, run_id):
+    return Path(f'/mnt/ceph/tira/data/runs/{dataset_id}/{user_id}/{run_id}/')
+
 def ensure_run_did_run_on_gammaweb09(user_id, dataset_id, run_id):
-    run_dir = Path(f'/mnt/ceph/tira/data/runs/{dataset_id}/{user_id}/{run_id}/profiling.zip')
+    run_dir = _run_dir(user_id, dataset_id, run_id) / 'profiling.zip'
     cpuinfo = ProfilingIntegration(None)._read_file_from_profiling_zip(run_dir, 'cpuinfo')
     nvidia_smi = ProfilingIntegration(None)._read_file_from_profiling_zip(run_dir, 'nvidia-smi.out')
 
@@ -16,8 +20,23 @@ def ensure_run_did_run_on_gammaweb09(user_id, dataset_id, run_id):
         raise ValueError('Did not run on gammaweb09: ', run_dir)
 
 
+def zip_runs(paths_to_be_zipped, name):
+    """ Copied from "from tira.views import zip_run, zip_runs" to run this without the tira server dependencies """
+    
+    zipped = Path(f"{name}.zip")
+    if zipped.exists():
+        return
+
+    with zipfile.ZipFile(zipped, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for path_to_be_zipped in paths_to_be_zipped:
+            path_to_be_zipped = Path(path_to_be_zipped)
+            for f in path_to_be_zipped.rglob('*'):
+                zipf.write(f, arcname=f.relative_to(path_to_be_zipped.parent))
+
+    return zipped
+
 def aggregation_of_run(user_id, dataset_id, run_id):
-    run_dir = Path(f'/mnt/ceph/tira/data/runs/{dataset_id}/{user_id}/{run_id}/')
+    run_dir = _run_dir(user_id, dataset_id, run_id)
     profiling = ProfilingIntegration(None).from_local_run_output(run_dir)
 
     elapsed_time = profiling[-1]
@@ -40,13 +59,27 @@ def aggregation_of_run(user_id, dataset_id, run_id):
 if __name__ == '__main__':
     runs = json.load(open('runs.json'))
     ret = []
+    
+    groups_for_archival = {}
+    
     for i in runs:
         task, user_id, approach = i.split('/')
+        group = runs[i]['class'] if 'class' in runs[i] else f'{user_id}-{approach}'.lower().replace(' ', '-').replace('(', '').replace(')', '')
+        
+        if group not in groups_for_archival:
+            groups_for_archival[group] = {}
+        
         for dataset_id, run_id in runs[i]['runs'].items():
+            assert run_id not in groups_for_archival[group]
+            groups_for_archival[group][run_id] = _run_dir(user_id, dataset_id, run_id)
             ensure_run_did_run_on_gammaweb09(user_id, dataset_id, run_id)
             tmp = {'approach': i, 'dataset': dataset_id, 'run_id': run_id}
             tmp.update(aggregation_of_run(user_id, dataset_id, run_id))
             ret += [tmp]
 
     pd.DataFrame(ret).to_json('aggregated-profiling.jsonl', lines=True, orient='records')
+    
+    for archive_group in tqdm(groups_for_archival, 'Zip runs'):
+        runs_to_zip = sorted(list(groups_for_archival[archive_group].values()))
+        zip_runs(archive_group, runs_to_zip)
 
